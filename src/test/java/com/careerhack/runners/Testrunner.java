393cf23983a4332f6e392216a2e8ce545161cc90
nusaibaramisa <nusaibaramisa@10.67.228.23>
2022-03-29 21:06:38 -0400

    updated spacing

--- src/test/java/com/careerhack/runners/Testrunner.java
+++ src/test/java/com/careerhack/runners/Testrunner.java
@@ -4,6 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+		//updated the spacing
+		
          features = "Features",
          glue = "com.careerhack.steps"
     )     
